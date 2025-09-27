073dd9eca420b1ec6a9f9df762c76ba68d47ca35
NandaGarin <nandagarin2012@gmail.com>
2025-09-27 19:05:33 +0700

    add addres and ethereum in card

--- client/krypt/src/components/Welcome.jsx
+++ client/krypt/src/components/Welcome.jsx
@@ -54,9 +54,21 @@ const Welcome = () => {
                             <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
                                 <SiEthereum fontSize={21} color="#fff"/>
                             </div>
+                            <BsInfoCircle fontSize={17} color="#fff"/>
+                        </div>
+                        <div>
+                            <p className="text-white font-light text-sm">
+                                Address
+                            </p>
+                            <p className="text-white font-semibold text-lg mt-1">
+                               Ethereum
+                            </p>
                         </div>
                     </div>
                 </div>
+
+
+                <div className="p-5 sm:w-96 w-full"></div>
             </div>
 
             </div>
